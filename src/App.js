e8961ab9b3d410eebd24ad7bd04f1e0edd2dfc53
one <juunone@uneedcomms.com>
2018-08-02 18:04:14 +0900

    app.css  추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import "./App.css"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>12313</p>
-      </div>
+      <Fragment>
+          <Button danger />
+          <Button />
+      </Fragment>
     );
   }
 }
+
+const Button = ({ danger }) => (
+  <button className={danger ? "button button--danger" : "button button--success"}>
+  Hello !
+  </button>
+)
 
 export default App;

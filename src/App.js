ae5cc7a88dcc0a922376f8ff49a946c507d8bcd1
one <juunone@uneedcomms.com>
2018-08-03 17:12:14 +0900

    스타일컴퍼넌트 마무리으

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component, Fragment } from 'react';
-import "./App.css"
+import React, { Component } from 'react';
+import styled, { injectGlobal, ThemeProvider } from "styled-components";
+import theme from './theme';
+
+injectGlobal`
+  body{
+    padding:0;
+    margin:0;
+  }
+`
+
+const Card = styled.div`
+  background-color:white;
+`
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+  ${Card}{
+    background-color : blue;
+  }
+`;
+
+const Button = styled.button`
+  border-radius:20px;
+  width:100px;
+  padding:5px;
+  background-color : ${props => props.theme.successColor};
+`
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-          <Button danger />
-          <Button />
-      </Fragment>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Form />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
 
-const Button = ({ danger }) => (
-  <button className={danger ? "button button--danger" : "button button--success"}>
-  Hello !
-  </button>
+const Form = () => ( 
+  <Card>
+    <Button>
+      123
+    </Button>
+  </Card>
 )
 
 export default App;
